c8378fb6418862cc55fe8b3b4c6736465841da39
Vivek <vivek.15@mountblue.tech>
2021-04-13 17:24:36 +0530

    Added router and connect sequelize

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const db = require("./models");
+const authRoute = require('./routes/authenticate');
 
 dotenv.config();
 
@@ -9,10 +10,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use("/user",authRoute)
+
 
 db.sequelize
     .sync()
-    .then((req) => {
+    .then(() => {
         app.listen(PORT, () => {
             console.log(`PORT is ${PORT}`);
         });
