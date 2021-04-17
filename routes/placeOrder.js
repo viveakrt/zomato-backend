b89c4cdb164e419c24e152bfd1be7001b8e59d25
Vivek <vivek.15@mountblue.tech>
2021-04-17 09:50:42 +0530

    BUG fixed on line 41

--- routes/placeOrder.js
+++ routes/placeOrder.js
@@ -8,7 +8,6 @@ const {
 
 const router = require("express").Router();
 
-<<<<<<< HEAD
 router.put('/placeOrder', verify, (req, res) => {
     const val = req.body.items;
     placeOrder.create({
@@ -57,56 +56,6 @@ router.post('/myOrder', verify, (req, res) => {
 
             res.sendStatus(500).end();
         });
-=======
-router.put('/placeOrder',verify,(req,res)=>{
-    const val = req.body.items;
-    placeOrder.create({
-        order_time: new Date(),
-        restaurant_id: req.body.restaurantId,
-        customer_id: req.body.customerId,
-        price: req.body.price,
-        comment: req.body.comment,
-        address: req.body.address
-    }).then((data) => {
-        Object.keys(val).forEach(element => {
-            inOrder.create({
-                PlaceOrder_id:data.dataValues.id_place_order,
-                Food_item_id:Number(element),
-                quantity:val[element],
-            })
-        });
-            res.status(200).json({
-                message:"ORDER PLACED"
-            }).end();
-    })
-    .catch((err) => {
-        console.log(err);
-
-        res.status(500).json({
-            message:'Internal server error'
-        }).end();
-    });
-})
-
-router.get('/myOrder',verify,(req,res)=>{
-    const id = req.body.customerId;
-    placeOrder.findAll({
-        where:{
-            customer_id:id
-        }
-    }).then((order) => {
-        if (order.length > 0) {
-            res.status(200).json(order).end();
-        } else {
-            res.sendStatus(404).end();
-        }
-    })
-    .catch((err) => {
-        console.log(err);
-
-        res.sendStatus(500).end();
-    });
->>>>>>> 03d0e766317ab78390d361441beb35cd070e7a9a
 })
 
 
